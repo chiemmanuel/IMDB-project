a103242820bd3397caf1339bfa3f89ab66108015
Travis-Berthrong <33703020+Travis-Berthrong@users.noreply.github.com>
2024-12-22 10:29:54 -0800

    added explanations to webpage queries and db schema

--- webpages_generation_queries/select_episode_page.sql
+++ webpages_generation_queries/select_episode_page.sql
@@ -1,14 +1,29 @@
 
--- Query to fetch all episode-specific information for a given episode ID
-SELECT parent_ti.title_id AS parent_id, parent_ti.primary_title AS parent_title, ep_ti.primary_title AS episode_title, ep_ti.original_title, ep_ti.is_adult, ep_ti.start_year AS release_year, ep_ti.runtime_minutes, ep_ti.genres,
-te.season_number, te.episode_number, ep_tr.average_rating, ep_tr.num_votes
+-- Query to fetch episode-specific information for a given episode ID
+-- This main query fetches all the basic information related to a given episode, such as the parent's title & id, title, release year, runtime, genres, season number, episode number, average rating, and number of votes.
+SELECT parent_ti.title_id AS parent_id, parent_ti.primary_title AS parent_title,
+    ep_ti.primary_title AS episode_title, ep_ti.original_title, ep_ti.is_adult,
+    ep_ti.start_year AS release_year, ep_ti.runtime_minutes, ep_ti.genres,
+    te.season_number, te.episode_number, ep_tr.average_rating, ep_tr.num_votes
 FROM title_episodes te
-JOIN title_info ep_ti ON ep_ti.title_id = 'tt0041951'
-JOIN title_ratings ep_tr ON ep_tr.title_id = 'tt0041951'
+JOIN title_info ep_ti ON ep_ti.title_id = te.episode_id
+JOIN title_ratings ep_tr ON ep_tr.title_id = te.episode_id
 JOIN title_info parent_ti ON parent_ti.title_id = te.parent_title_id
 WHERE te.episode_id = 'tt0041951';
 
--- Query to fetch all actors who appeared in the given episode but not in any other episode of the same series
+-- Query to fetch the writers & directors for a given episode ID
+-- This query fetches the writers & directors for a given episode by joining the crew_members table with the person_info table on the person_id field.
+-- It then filters the results based on the title_id and the category (writer or director) to get the desired information.
+-- The results are ordered by the category to separate the writers from the directors.
+SELECT cm.category, cm.person_id, pi.primary_name
+FROM crew_members cm
+JOIN person_info pi ON cm.person_id = pi.person_id
+WHERE cm.title_id = 'tt0041951' AND (cm.category = 'writer' OR cm.category = 'director')
+ORDER BY cm.category;
+
+-- Query to fetch guest stars for a given episode ID
+-- As guest star is not a standard role in the IMDB dataset, I instead chose to determine guest stars as actors who only appear in a single episode of a given series.
+-- I find these actors by filtering out actors who appear in any title that shares the same parent title ID as the given episode, but are not the given episode itself.
 SELECT DISTINCT
     ca.person_id,
     pi.primary_name AS actor_name,
--- webpages_generation_queries/select_person_page.sql
+++ webpages_generation_queries/select_person_page.sql
@@ -4,17 +4,21 @@ FROM person_info pi
 WHERE pi.person_id = 'nm0103842';
 
 -- Select info for most popular titles of a person
+-- This query fetches the titles where the person is known for, based on the known_for_titles array in the person_info table.
+-- This would provide the info for a banner or carousel of the person's most popular titles.
 SELECT ti.*, tr.average_rating, tr.num_votes
 FROM title_info ti
 JOIN title_ratings tr 
 ON tr.title_id = ti.title_id
 WHERE ti.title_id IN (
-    SELECT unnest(known_for_titles) 
+    SELECT unnest(known_for_titles) -- unnest() function is used to unpack the array into individual rows for the IN clause
     FROM person_info 
     WHERE person_id = 'nm0103842'
 );
 
 -- Select all titles where the person is an actor
+-- This query fetches all the titles where the person is an actor, along with the average rating and number of votes for each title.
+-- It would be used to display a more exhasutive list of the person's acting credits.
 SELECT ti.*, tr.average_rating, tr.num_votes
 FROM crew_actors ca
 JOIN title_info ti ON ca.title_id = ti.title_id
@@ -22,6 +26,8 @@ JOIN title_ratings tr ON tr.title_id = ti.title_id
 WHERE ca.person_id = 'nm0103842';
 
 -- Select all titles where the person is a crew member
+-- This query fetches all the titles where the person is a crew member, along with the category of their role (e.g., writer, director) and the average rating and number of votes for each title.
+-- It would be used to display a list of the person's non-acting credits.
 SELECT cm.category, ti.*, tr.average_rating, tr.num_votes
 FROM crew_members cm
 JOIN title_info ti ON cm.title_id = ti.title_id
